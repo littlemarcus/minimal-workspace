f03080de57716150cfa8ad459e3dfc06837a504c
Marcus Little <mcolelittle@gmail.com>
2024-05-24 04:48:05 -0400

    initial commit

---
+++ apps/gatsby-site/src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Hello, world!</h1>
+    </main>
+  );
+}
+
+export const Head = () => <title>Home Page</title>
+
+export default IndexPage;
